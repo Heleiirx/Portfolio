30b65b1e6598d0e4741f88cd2d6fd5dffa1e99c0
Heleir <itzelvargas2002@gmail.com>
2023-06-30 16:56:23 -0500

    Limpieza de codigo js y html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-document.getElementById("portada").click();
-// function cargar() {
-//   console.log(portada);
-//   portada.click();
-// }
-// portada.addEventListener("onload", cargar);
+// document.getElementById("portada").click();
+
 //--------------------------Habilitar boton contacto-----------------------------------------
 const botonContacto = document.getElementById("botonSubmit");
 const textArea = document.getElementById("textarea");
@@ -11,24 +7,13 @@ const textArea = document.getElementById("textarea");
 function habilitarBoton(){
   const inputs = document.querySelectorAll("input");
   console.log("Input" + JSON.stringify(inputs,null,2));
-  
-  // let input = ;
-  // console.log(input.value);
-  
-//--------------------------Cambiar slide-----------------------------------------
 
+//--------------------------Cambiar slide-----------------------------------------
   if (inputs[0].value && inputs[1].value && inputs[2].value && textArea.value) {
     console.log(inputs);
         botonContacto.removeAttribute("disabled");
       }
     }
-
-    // const heart = document.querySelectorAll("i");
-      
-    //   heart.forEach(i =>{
-    //     i.addEventListener("click", cambioSlide);
-    //     console.log(heart);
-    //   })
 
     function cambioSlide(desplazamiento, deg, event){
       let activeHeart = document.querySelector(".bi-suit-heart-fill");
@@ -45,12 +30,3 @@ function habilitarBoton(){
       // carousel.style.animationPlayState="paused";
       carousel.style.transform= `translateZ(${desplazamiento}) rotateY(${deg})`;    
     }
-//------------------------------------Botones skills-----------------------------------------
-      
-      
-//       function estiloCorazon(event){
-//         const fullHeart = event.target;
-//         fullHeart.classList.toggle("bi-suit-heart-fill");
-//         fullHeart.classList.toggle("bi-suit-heart");
-//       }
-// }
