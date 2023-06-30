9c80342ed114ff2783c154af1842f8daedb889b7
Heleir <itzelvargas2002@gmail.com>
2023-06-29 21:37:18 -0500

    Heart skills

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+//------------------------------------Botones skills-----------------------------------------
+const heart = document.querySelectorAll("i");
+
+heart.forEach(i =>{
+  i.addEventListener("click", cambioSlide);
+  console.log(heart);
+})
+
+function cambioSlide(event){
+  const fullHeart = event.target;
+  fullHeart.classList.toggle("bi-suit-heart-fill");
+  fullHeart.classList.toggle("bi-suit-heart");
+}
+
+//--------------------------Habilitar boton contacto-----------------------------------------
 const botonContacto = document.getElementById("botonSubmit");
 
 function habilitarBoton(){
