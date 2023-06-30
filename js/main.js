68b0a7cf4e7a1ebabfc82807a7540cf390004a23
Heleir <itzelvargas2002@gmail.com>
2023-06-30 16:23:43 -0500

    Botones heart skills

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
-//------------------------------------Botones skills-----------------------------------------
-const heart = document.querySelectorAll("i");
-
-heart.forEach(i =>{
-  i.addEventListener("click", cambioSlide);
-  console.log(heart);
-})
-
-function cambioSlide(event){
-  const fullHeart = event.target;
-  fullHeart.classList.toggle("bi-suit-heart-fill");
-  fullHeart.classList.toggle("bi-suit-heart");
-}
 
 //--------------------------Habilitar boton contacto-----------------------------------------
 const botonContacto = document.getElementById("botonSubmit");
+const textArea = document.getElementById("textarea");
 
 function habilitarBoton(){
-  const input = document.querySelectorAll("input");
-  console.log(input);
+  const inputs = document.querySelectorAll("input");
+  console.log("Input" + JSON.stringify(inputs,null,2));
+  
+  // let input = ;
+  // console.log(input.value);
+  
+//--------------------------Cambiar slide-----------------------------------------
 
-  botonContacto.removeAttribute("disabled");
+  if (inputs[0].value && inputs[1].value && inputs[2].value && textArea.value) {
+    console.log(inputs);
+        botonContacto.removeAttribute("disabled");
+      }
+    }
 
-}
-habilitarBoton();
+    // const heart = document.querySelectorAll("i");
+      
+    //   heart.forEach(i =>{
+    //     i.addEventListener("click", cambioSlide);
+    //     console.log(heart);
+    //   })
+
+    function cambioSlide(desplazamiento, deg, event){
+      let activeHeart = document.querySelector(".bi-suit-heart-fill");
+      if (activeHeart) {
+        activeHeart.classList.remove("bi-suit-heart-fill");
+        activeHeart.classList.add("bi-suit-heart");
+      }
+      event.target.classList.remove("bi-suit-heart");
+      event.target.classList.add("bi-suit-heart-fill");
+      
+
+      let carousel = document.getElementById("carousel");
+      carousel.style.animation="none";
+      // carousel.style.animationPlayState="paused";
+      carousel.style.transform= `translateZ(${desplazamiento}) rotateY(${deg})`;    
+    }
+//------------------------------------Botones skills-----------------------------------------
+      
+      
+//       function estiloCorazon(event){
+//         const fullHeart = event.target;
+//         fullHeart.classList.toggle("bi-suit-heart-fill");
+//         fullHeart.classList.toggle("bi-suit-heart");
+//       }
+// }
