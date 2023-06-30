e44d9c2b0e50f517aabf79c6552e6da796bc07ce
Heleir <itzelvargas2002@gmail.com>
2023-06-29 19:42:30 -0500

    Adicion de animaciones, footer y validacion inputs

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-var carousel = $(".carousel"),
-    currdeg  = 0;
+const botonContacto = document.getElementById("botonSubmit");
 
-$(".next").on("click", { d: "n" }, rotate);
-$(".prev").on("click", { d: "p" }, rotate);
+function habilitarBoton(){
+  const input = document.querySelectorAll("input");
+  console.log(input);
 
-function rotate(e){
-  if(e.data.d=="n"){
-    currdeg = currdeg - 60;
-  }
-  if(e.data.d=="p"){
-    currdeg = currdeg + 60;
-  }
-  carousel.css({
-    "-webkit-transform": "rotateY("+currdeg+"deg)",
-    "-moz-transform": "rotateY("+currdeg+"deg)",
-    "-o-transform": "rotateY("+currdeg+"deg)",
-    "transform": "rotateY("+currdeg+"deg)"
-  });
+  botonContacto.removeAttribute("disabled");
+
 }
+habilitarBoton();
